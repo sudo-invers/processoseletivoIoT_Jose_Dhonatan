99ea15f18130dd9a7bac7b6f92f6f007a6d45771
sudo-invers <dhonatanjose8@gmail.com>
2026-04-25 16:21:54 +0000

    feat:Added buttons and the pet status and simples functions

---
+++ src/buttons.py
@@ -0,0 +1,38 @@
+from machine import Pin
+import time
+
+
+class Buttons:
+    def __init__(self):
+
+        self.feed_btn = Pin(14, Pin.IN, Pin.PULL_UP)
+        self.play_btn = Pin(27, Pin.IN, Pin.PULL_UP)
+        self.sleep_btn = Pin(26, Pin.IN, Pin.PULL_UP)
+
+        self.last_press = 0
+
+    def debounce(self):
+
+        now = time.ticks_ms()
+
+        if time.ticks_diff(now, self.last_press) < 250:
+            return False
+
+        self.last_press = now
+        return True
+
+    def read(self):
+
+        if not self.feed_btn.value():
+            if self.debounce():
+                return "feed"
+
+        if not self.play_btn.value():
+            if self.debounce():
+                return "play"
+
+        if not self.sleep_btn.value():
+            if self.debounce():
+                return "sleep"
+
+        return None
--- src/main.py
+++ src/main.py
@@ -1,1 +1,34 @@
-print("Teste")
+from pet import Pet
+from buttons import Buttons
+import time
+
+pet = Pet("Byte")  # He is just a little byte
+buttons = Buttons()
+
+UPDATE_INTERVAL = 3000
+last_update = time.ticks_ms()  # 1 sec = 1000 ticks
+
+print("Cyber Pet")
+
+while pet.alive:
+    now = time.ticks_ms()
+
+    if time.ticks_diff(now, last_update) >= UPDATE_INTERVAL:
+        pet.update()
+        pet.show_status()
+        last_update = now
+
+    action = buttons.read()
+
+    if action == "feed":
+        pet.feed()
+
+    elif action == "play":
+        pet.play()
+
+    elif action == "sleep":
+        pet.sleep()
+
+    time.sleep_ms(50)
+
+print("Game Over.")
